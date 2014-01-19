a8f890fd848cbbc5f86cf93df2f436aef736d59f
gxw <alphaguoxiongwei@gmail.com>
2014-01-19 19:24:28 +0800

    增加本地通知

--- guide-demo/webViewController.m
+++ guide-demo/webViewController.m
@@ -31,6 +31,28 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    UILocalNotification *notification=[[UILocalNotification alloc] init];
+    if (notification!=nil) {
+        
+        NSDate *now=[NSDate new];
+        notification.fireDate=[now dateByAddingTimeInterval:6]; //触发通知的时间
+        notification.repeatInterval=0; //循环次数，kCFCalendarUnitWeekday一周一次
+        
+        notification.timeZone=[NSTimeZone defaultTimeZone];
+        notification.soundName = UILocalNotificationDefaultSoundName;
+        notification.alertBody=@"该去吃晚饭了！";
+        
+        notification.alertAction = @"打开";  //提示框按钮
+        notification.hasAction = YES; //是否显示额外的按钮，为no时alertAction消失
+        
+        notification.applicationIconBadgeNumber = 1; //设置app图标右上角的数字
+        
+        //下面设置本地通知发送的消息，这个消息可以接受
+        NSDictionary* infoDic = [NSDictionary dictionaryWithObject:@"value" forKey:@"key"];
+        notification.userInfo = infoDic;
+        //发送通知
+        [[UIApplication sharedApplication] scheduleLocalNotification:notification];
+    }
 }
 
 - (void)didReceiveMemoryWarning
